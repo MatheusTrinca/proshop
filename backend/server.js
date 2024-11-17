a277a4b22f516fc7d86aeecbf2d1af1a3d26341f
Matheus <matheusfelipetrinca@gmail.com>
2024-11-17 16:10:55 -0300

    feat: adding-routes-and-error

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
+
+// Connecting to MongoDB
+connectDB();
 
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.send(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.send(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
