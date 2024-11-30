b06c4c717fc726b4df31a6c90a13cfe61054dce0
Matheus <matheusfelipetrinca@gmail.com>
2024-11-30 02:36:44 -0300

    feat: validating-user-password

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,18 @@ const port = process.env.PORT || 5000;
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 // Connecting to MongoDB
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
