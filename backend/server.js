63cec48d1cd387467cabf2efaa48ac65af0e8149
Matheus <matheusfelipetrinca@gmail.com>
2024-12-07 17:20:40 -0300

    feat: adding-cookies-and-profile-routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
 // Connecting to MongoDB
 connectDB();
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
