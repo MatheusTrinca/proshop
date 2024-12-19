9dcc6e86fbc4eae83d0275c1f9d9bf7b077a5fa7
Matheus <matheusfelipetrinca@gmail.com>
2024-12-18 21:15:45 -0300

    feat: adding-shipping-screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -24,12 +24,18 @@ const cartSlice = createSlice({
       return updateCart(state);
     },
 
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
+    },
+
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter(x => x._id !== action.payload);
     },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
