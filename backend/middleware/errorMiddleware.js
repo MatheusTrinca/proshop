da9d632a897ebd42cb4c340067c03c776aad6eb6
Matheus <matheusfelipetrinca@gmail.com>
2024-12-29 04:49:50 -0300

    fix: adjusting-small-fixes

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -7,11 +7,6 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
-  if (err.name === 'CastError' && err.kind === 'ObjectId') {
-    statusCode = 404;
-    err.message = 'Resource not found';
-  }
-
   res.status(statusCode);
   res.json({
     message: err.message,
